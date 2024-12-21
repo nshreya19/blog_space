913daa333700df3305857c6f3a5a7c3f251886aa
Shreya N <167218342+nshreya19@users.noreply.github.com>
2024-12-21 20:24:09 +0530

    Add files via upload

---
+++ blog_project/blog/views.py
@@ -0,0 +1,42 @@
+
+# Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
+from django.contrib.auth.decorators import login_required
+from . import views
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+@login_required
+def post_create(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        Post.objects.create(title=title, content=content, author=request.user)
+        return redirect('post_list')
+    return render(request, 'blog/post_form.html')
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.title = request.POST['title']
+        post.content = request.POST['content']
+        post.save()
+        return redirect('post_detail', pk=post.pk)
+    return render(request, 'blog/post_form.html', {'post': post})
+
+@login_required
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'blog/post_confirm_delete.html', {'post': post})
+
